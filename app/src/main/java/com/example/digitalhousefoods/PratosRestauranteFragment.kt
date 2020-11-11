d4b44ffe48998270881127ec922523ac5b5982a6
Giulia Machado <giuliamachado03@gmail.com>
2020-11-11 19:06:11 -0300

    Design Fragments Menus

--- app/src/main/java/com/example/digitalhousefoods/PratosRestauranteFragment.kt
+++ app/src/main/java/com/example/digitalhousefoods/PratosRestauranteFragment.kt
@@ -5,16 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
-
-class RestaurantesFragment : Fragment() {
+class PratosRestauranteFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
         }
     }
 
@@ -23,14 +18,7 @@ class RestaurantesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurantes, container, false);
-
-
-
-
-
+        return inflater.inflate(R.layout.fragment_pratos_restaurante, container, false)
     }
 
-
 }
-
