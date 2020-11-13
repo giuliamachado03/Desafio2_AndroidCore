eed0c067fbd57de8dcc9926a12e1125abc8ddc3a
Giulia Machado <giuliamachado03@gmail.com>
2020-11-12 21:38:45 -0300

    Navegação login - register

--- app/src/main/java/com/example/digitalhousefoods/login/LoginFragment.kt
+++ app/src/main/java/com/example/digitalhousefoods/login/LoginFragment.kt
@@ -5,17 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import com.example.digitalhousefoods.R
 
 class LoginFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,5 +19,18 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            navController.navigate(R.id.restaurantesFragment)
+        }
+
+        view.findViewById<Button>(R.id.btnRegisterLogin).setOnClickListener {
+            navController.navigate(R.id.registerFragment)
+        }
+    }
 
 }
